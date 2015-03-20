e9fa70a0ae65f26d15a16f844ac1cf965ffc52d7
sinaghaffari <sina.ghaffari321@gmail.com>
2015-03-20 17:32:04 -0400

    Made the snake_body single dimensional and added keyboardController.v and .gitignore.

--- snake.v
+++ snake.v
@@ -36,6 +36,7 @@ module snake(
 	wire CLK = ~KEY[0];
 	wire [29999:0] snake_wire = snake_body;
 	reg [29999:0] snake_body;
+	reg lose;
 	assign LEDR = snake_body[11:0];
 	/*always@(posedge CLK)begin
 		case(keyValue)
@@ -58,7 +59,6 @@ module snake(
 	/* snake_length_bitstring has one bit for each of the segments of our snake. If the snake is n segments long, snake_length_bitstring will consist of (n - 1) one's, followed by (2499 - n) zeroes.
 	 * This will be used to determine if the shift register should pass on the value of one segment to the next.
 	 */
-	reg [2499:0] snake_length_bitstring;
 	reg [11:0] snake_length;
 	reg [5:0] new_head_x; // Represents the current x coordinate of the head of the snake.
 	reg [5:0] new_head_y; // Represents the current y coordinate of the head of the snake.
@@ -67,45 +67,62 @@ module snake(
 	reg [1:0] dy; // Is the snake moving up or down?
 	
 	initial begin // Initialize everything to 0.
-		snake_length_bitstring = 2500'b0;
 		snake_direction = 2'b0; // Initialize snake moving upwards.
 		snake_length = 12'b000000000001; 
 		snake_body = 30000'b0;
-		
-		snake_body[11:0] =  12'b011001011001;
+		lose = 1'b0;
+		snake_body[11:0] =  12'b011001011001; // Set the current head of the snake to b the centre of the screen.
 	end
 	reg [16:0] i;
 	always@( posedge CLK ) begin
-		snake_body[2499] = 12'b0; // No matter what, the tail of our shift register will drop it's current value.
-		for (i = 2499; i > 0; i = i - 12) begin
-			//1 d this, also what about the event that we 'hit' a tail, im going to hardcode it in, but i cant seem to find it >.>,
-			//can you hard code a tail and show me where you did it.
+	// Handle the last snake block.
+		snake_body[29999:29988] = 12'b0; // No matter what, the tail of our shift register will drop it's current value.
+	
+	// Handle everything other than the last and first snake block.
+		for (i = 2498; i > 0; i = i - 1) begin
+			snake_body[i * 12 + 11:i * 12] = snake_body[i*12 - 1:i*12 - 12] & (snake_length < (i - 1));
 		end
-		// I wouldnt worry too much about multiple key inputs etc... keep it simple right now, otherwise we'll shoot outselves in the foot
-		if ( keyValue == 117 ) begin // Change Up.
+
+	// Handle the first snake block.
+		if ( keyValue == 117 & ~snake_direction == 2'b11 ) begin // Change Up if not going Down..
 			snake_direction = 2'b0;
-		end else if ( keyValue == 116 ) begin // Change Right.
+		end else if ( keyValue == 116 & ~snake_direction == 2'b10) begin // Change Right if not going Left.
 			snake_direction = 2'b01;
-		end else if ( keyValue == 114 ) begin // Change Down.
+		end else if ( keyValue == 114 & ~snake_direction == 2'b00) begin // Change Down if not going Up.
 			snake_direction = 2'b11;
-		end else if ( keyValue == 107 ) begin // Change Left.
+		end else if ( keyValue == 107 & ~snake_direction == 2'b01) begin // Change Left if not going Right.
 			snake_direction = 2'b10;
 		end
 		
 		if (snake_direction == 2'sb0) begin // Up.
-			snake_body[11:6] = snake_body[11:6] - 1;
+			new_head_x = snake_body[11:6] + 0;
+			new_head_y = snake_body[5:0] - 1;
 		end else if (snake_direction == 2'sb01) begin // Right.
-			snake_body[5:0] = snake_body[5:0] + 1;
+			new_head_x = snake_body[11:6] + 1;
+			new_head_y = snake_body[5:0] + 0;
 		end else if (snake_direction == 2'sb11) begin // Down.
-			snake_body[11:6] = snake_body[11:6] + 1;
+			new_head_x = snake_body[11:6] + 0;
+			new_head_y = snake_body[5:0] + 1;
 		end else if (snake_direction == 2'sb10) begin // Left.
-			snake_body[5:0] = snake_body[5:0] - 1;
-			
-		
+			new_head_x = snake_body[11:6] - 1;
+			new_head_y = snake_body[5:0] + 0;
 		end
-		//for some reason the signed bit addition isnt working, so changing the snakebody's abovce
-		//snake_body[11:0] = {new_head_x[5:0] , new_head_y[5:0]}; //giving me zig zags, no idea why, changed it back.
 
+		// Check to see if you hit the borders.
+		if (new_head_x < 0 | new_head_x >= 50 | new_head_y < 0 | new_head_y >= 50) begin
+			lose = 1'b1;
+		end
+
+		// Check for snake on snake collision.
+		// Having a dedicated loop for it may be a little bit inefficient. It would be nice to be able to combine it with the above loop.
+		for (i = 1; i < snake_length; i = i + 1) begin
+			if (new_head_x == snake_body[i*12+11:i*12 + 6] | new_head_y == snake_body[i*12+5:i*12]) begin
+				lose = 1'b1;
+			end
+		end
+
+		snake_body[11:6] = new_head_x;
+		snake_body[5:0] = new_head_y;
 	end	
 endmodule
 
